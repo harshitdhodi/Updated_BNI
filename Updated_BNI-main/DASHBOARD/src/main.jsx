c77e123448d26ecd47dbf12aeda7d3513d7a7460
harshitdhodi <harshit.dhodi2108@gmail.com>
2025-11-20 22:25:23 +0530

    convert it into pwa web app

--- Updated_BNI-main/DASHBOARD/src/main.jsx
+++ Updated_BNI-main/DASHBOARD/src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { registerSW } from 'virtual:pwa-register'
 
+registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {}
+})
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App className ="bg-gray-200" />
+    <App className="bg-gray-200" />
   </React.StrictMode>
 );
